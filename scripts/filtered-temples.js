8178b2e5f6667e69cee7d9d8cba7d41ce0f6cba3
Jeniffer Carrillo Ponton <jcarrillo1@byupathway.edu>
2026-07-27 02:50:03 -0500

    Use clearer Mexico temple photo

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -46,7 +46,7 @@ const temples = [
     location: "Mexico City, Mexico",
     dedicated: "1983, December, 2",
     area: 116642,
-    imageUrl: "images/temples/mexico-temple.jpg"
+    imageUrl: "images/temples/mexico-city.jpg"
   },
   {
     templeName: "Guayaquil Ecuador Temple",
